80568c04ec523ff051680b83b4d7fdef1631c9c2
Andy Spohn <Andy@AndySpohn.com>
2016-03-15 14:33:10 -0400

    Add an env var to set cmdline options

--- run-jenkins.sh
+++ run-jenkins.sh
@@ -6,5 +6,6 @@
 # https://cloudbees.zendesk.com/hc/en-us/articles/204690520-Why-do-slaves-show-as-suspended-while-jobs-wait-in-the-queue-
 USE_DEFAULTS="-Dhudson.slaves.NodeProvisioner.MARGIN=40 -Dhudson.slaves.NodeProvisioner.MARGIN0=0.75 -Xms512m -Xmx2g -Djava.awt.headless=true -Dorg.apache.commons.jelly.tags.fmt.timeZone=America/New_York"
 JENKINS_JAVA_OPTIONS=${JENKINS_JAVA_OPTIONS-$USE_DEFAULTS}
+JENKINS_CMDLINE_OPTIONS=${JENKINS_CMDLINE_OPTIONS-}
 
-java "${JENKINS_JAVA_OPTIONS}" -jar /usr/lib/jenkins/jenkins.war
+java "${JENKINS_JAVA_OPTIONS}" -jar /usr/lib/jenkins/jenkins.war "${JENKINS_CMDLINE_OPTIONS}"
